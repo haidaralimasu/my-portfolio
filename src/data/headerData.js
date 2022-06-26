913831522db0e938aec3459e79e50ce268771afc
Haidarali Masu <haidaralimasu123@gmail.com>
2022-06-26 08:07:22 +0530

    changed imahe

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -6,6 +6,6 @@ export const headerData = {
   desciption:
     "There wasn't a bird in the sky, but that was not what caught his attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. He knew what was coming and He hoped He was prepared. ",
   image:
-    "https://raw.githubusercontent.com/PhantomScript/asset-container/main/developer-portfolio/landingImg.png",
+    "https://avatars.githubusercontent.com/u/80143769?s=400&u=fad1f532ee7d9d2579a82346a94d2291b10bef47&v=4",
   resumePdf: resume,
 };
